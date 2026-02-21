b890a9c0c59c5de1c5d13f1c3869a53f49744cbe
Grey Ninja <152683729+wizardofcoding1@users.noreply.github.com>
2026-02-21 13:38:08 +0530

    Delete backend directory

--- backend/src/routes/onboardingRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const onboardingController = require("../controllers/onboardingController");
-
-router.post("/get-started", onboardingController.getStarted);
-
-module.exports = router;
