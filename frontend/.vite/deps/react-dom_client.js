0a2f0f63784137429601a5d63abd0dbfd4e9a303
wizardofcoding1 <152683729+wizardofcoding1@users.noreply.github.com>
2026-02-21 12:56:38 +0530

    Added The redirect Page

---
+++ frontend/.vite/deps/react-dom_client.js
@@ -0,0 +1,8 @@
+import {
+  require_client
+} from "./chunk-ZGZM5V3M.js";
+import "./chunk-6YOVXODW.js";
+import "./chunk-R3E4LXVO.js";
+import "./chunk-P6RTVJOB.js";
+import "./chunk-G3PMV62Z.js";
+export default require_client();
---
+++ frontend/.vite/deps/zustand.js
@@ -0,0 +1,14 @@
+import {
+  create,
+  useStore
+} from "./chunk-IJ2WD5CD.js";
+import {
+  createStore
+} from "./chunk-WBBMM7TZ.js";
+import "./chunk-P6RTVJOB.js";
+import "./chunk-G3PMV62Z.js";
+export {
+  create,
+  createStore,
+  useStore
+};
